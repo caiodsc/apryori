88a057ee2cbee14f0416d5461131e5b90d28c921
Caio <caio.dscamara@gmail.com>
2017-11-29 00:55:37 -0200

    Apriori aplicado à tabela de compras básica.

--- mercado.py
+++ mercado.py
@@ -9,13 +9,13 @@ import pprint
 
 pp = pprint.PrettyPrinter()
 
-dados = pd.read_csv('mercado.csv', header = None)
+dados = pd.read_csv('mercado2.csv', header = None)
 transacoes = []
-for i in range(0,10):
-    transacoes.append([str(dados.values[i,j]) for j in range(0,4)])
+for i in range(0,7501):
+    transacoes.append([str(dados.values[i,j]) for j in range(0,20)])
 
 from apryori import apriori
-regras = apriori(transacoes, min_support = 0.3, min_confidence = 0.8, min_lift = 2, min_length = 2)
+regras = apriori(transacoes, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
 resultados = list(regras)
 #resultados    
@@ -26,4 +26,5 @@ resultados2 = [list(x) for x in resultados]
 resultadoFormatado = []
 for j in range(0, 3):
     resultadoFormatado.append([list(x) for x in resultados2[j][2]])
+resultadoFormatado    
 pp.pprint(resultadoFormatado)
