1c9ba657c1020b6db6e251764d770ca88d9496b6
Caio <caio.dscamara@gmail.com>
2017-11-29 00:56:35 -0200

    Apriori aplicado a um supermercado.

--- mercado.py
+++ mercado.py
@@ -23,6 +23,7 @@ resultados = list(regras)
 resultados2 = [list(x) for x in resultados]
 #pp.pprint(resultados2)
 
+
 resultadoFormatado = []
 for j in range(0, 3):
     resultadoFormatado.append([list(x) for x in resultados2[j][2]])
